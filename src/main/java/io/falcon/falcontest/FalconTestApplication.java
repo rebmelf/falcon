9959afa36f94ca80e8767c747a8a05ca7feaf34b
Balazs Horvath <bala.horvath@gmail.com>
2018-01-29 13:42:55 +0100

    Initial setup of the gradle project with Spring Boot dependencies

---
+++ src/main/java/io/falcon/falcontest/FalconTestApplication.java
@@ -0,0 +1,12 @@
+package io.falcon.falcontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalconTestApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(FalconTestApplication.class, args);
+  }
+}
